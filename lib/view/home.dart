b60562d37d5fd8a174e82182943bfacb9a0d0f8c
Gustavo Peder <129289748+Gpeder@users.noreply.github.com>
2026-01-28 10:57:36 -0300

    view da lista de tarefas pronta

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,27 +1,18 @@
+import 'package:dayflow/components/home/listatarefas.dart';
 import 'package:dayflow/components/home/titulo.dart';
-import 'package:dayflow/theme/theme.dart';
-import 'package:dayflow/widgets/main_checkbox.dart';
-import 'package:dayflow/widgets/main_chip.dart';
 import 'package:dayflow/widgets/main_home_appbar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  static const categorias = [
-    'Todas',
-    'Trabalho',
-    'Saúde',
-    'Pessoal',
-    'Estudos',
-    'Outros',
-  ];
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MainHomeAppbar(),
-      body: SingleChildScrollView(
+      body: Padding(
         padding: .symmetric(horizontal: 16, vertical: 20),
         child: Column(
           crossAxisAlignment: .start,
@@ -29,45 +20,12 @@ class HomePage extends StatelessWidget {
             SizedBox(height: 10),
             TituloHomePage(),
             SizedBox(height: 10),
-            SizedBox(
-              height: 40,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: categorias.length,
-                separatorBuilder: (context, index) => SizedBox(width: 8),
-                itemBuilder: (context, index) {
-                  return MainChip(
-                    label: categorias[index],
-                    selected: index == 0,
-                    onTap: () {},
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 20),
-            ListTile(
-              leading: Container(
-                width: 5,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              title: Padding(
-                padding: const EdgeInsets.only(bottom: 5),
-                child: Text('Call com o time', style: AppTextStyles.text18Bold),
-              ),
-              subtitle: Text(
-                'Trabalho -  14:00',
-                style: AppTextStyles.text14.copyWith(
-                  color: AppColors.textMuted,
-                ),
-              ),
-              trailing: AppCheckbox(value: true, onChanged: (v) {}),
-            ),
+            ListaTarefas(),
           ],
         ),
       ),
     );
   }
 }
+
+
